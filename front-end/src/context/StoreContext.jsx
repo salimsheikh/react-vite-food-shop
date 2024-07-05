b31a068a70904e52c377a3106eb0b29497e41c11
Deepak Shah <deepak@infosofttech.com>
2024-07-05 19:45:23 +0530

    cart add and remove completed

--- front-end/src/context/StoreContext.jsx
+++ front-end/src/context/StoreContext.jsx
@@ -1,23 +1,49 @@
-import { createContext } from "react";
-import PropsTypes from 'prop-types'
+import { createContext, useEffect, useState } from "react";
+import PropsTypes from "prop-types";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-   const contextValue = {
+  
+
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = (itemId) => {
+    console.log(itemId)
+    if(!cartItems[itemId]){
+        setCartItems((prev) => ({...prev, [itemId]: 1}));
+    }else{
+        setCartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1}));
+    }
+  }
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({...prev, [itemId]: prev[itemId] - 1 }))
+  }
+
+  const contextValue = {
     // Add your context values here
-    food_list
-   }   
-   return(
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart
+  };
+
+  useEffect(() => {
+    console.log(cartItems)
+  },[cartItems])
+
+  return (
     <StoreContext.Provider value={contextValue}>
-        {props.children}
+      {props.children}
     </StoreContext.Provider>
-   );
-}
+  );
+};
 
 StoreContextProvider.propsTypes = {
-    children: PropsTypes.node.isRequired
-}
+  children: PropsTypes.node.isRequired,
+};
 
-export default StoreContextProvider
+export default StoreContextProvider;
